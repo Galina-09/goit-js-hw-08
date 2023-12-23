c14d721db5156693983bed7cf62c7a0cc697289d
Galina-09 <galinakryvoboks@gmail.com>
2023-12-23 08:01:28 +0200

    add struct

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const categories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categories.length}`);
-
-categories.forEach((item) => {
-  console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${item.lastElementChild.children.length}`);
-});
